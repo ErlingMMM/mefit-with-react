26094b0fafe384f800a1bc6d4861654c9963d33c
ErlingMMM <emagnusmoland@gmail.com>
2023-10-03 12:47:09 +0200

    refactor dont repeat yourself

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,21 +1,7 @@
-
 function Navbar({ setActiveComponent }: { setActiveComponent: (component: string) => void }) {
-
-
-  const switchToProfile = () => {
-    setActiveComponent('profile');
-  };
-
-  const switchToContributors = () => {
-    setActiveComponent('contributors');
-  };
-
-  const switchToGoalsDashboard = () => {
-    setActiveComponent('goals-dashboard');
-  };
-
-  const switchToGoalsDetails = () => {
-    setActiveComponent('goals-details');
+  
+  const switchToComponent = (component: string) => {
+    setActiveComponent(component);
   };
 
   return (
@@ -23,10 +9,10 @@ function Navbar({ setActiveComponent }: { setActiveComponent: (component: string
       <div className="bg-red-500 text-orange-300 p-4">
         <div className="user-indicator">User: John Doe</div>
         <div className="nav-links">
-          <button onClick={switchToProfile}>Profile</button>
-          <button onClick={switchToContributors}>Contributors Area</button>
-          <button onClick={switchToGoalsDashboard}>Goals Dashboard</button>
-          <button onClick={switchToGoalsDetails}>Goals Details</button>
+          <button onClick={() => switchToComponent('profile')}>Profile</button>
+          <button onClick={() => switchToComponent('contributors')}>Contributors Area</button>
+          <button onClick={() => switchToComponent('goals-dashboard')}>Goals Dashboard</button>
+          <button onClick={() => switchToComponent('goals-details')}>Goals Details</button>
         </div>
         <div className="logout-button">
           <button>Logout</button>
