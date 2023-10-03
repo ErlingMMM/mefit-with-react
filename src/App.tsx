0503c55aa559f436df913395b07d690a9ef0f442
ErlingMMM <emagnusmoland@gmail.com>
2023-10-03 09:27:07 +0200

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="bg-blue-500 text-red-300 p-4">
+  This is a div styled with Tailwind CSS.
+</div>
+      
     </div>
   );
 }
