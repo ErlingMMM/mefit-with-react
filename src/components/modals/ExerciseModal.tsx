144e234f1dffc888031faf893d8f3dcdc32567d6
ErlingMMM <emagnusmoland@gmail.com>
2023-10-10 11:46:08 +0200

    Move the h3 for "Description" under image

--- src/components/modals/ExerciseModal.tsx
+++ src/components/modals/ExerciseModal.tsx
@@ -22,26 +22,26 @@ function ExerciseModal({ isOpen, closeModal, exercise }: { isOpen: boolean, exer
               {exercise.name}
 
               <div className="mt-3 text-left">
-                <h3
-                  className="text-base font-semibold leading-6 text-black"
-                  id="modal-title"
-                >
-                  Description
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    {exercise.description}
-                  </p>
-                </div>
-                <div className="mt-2 max-h-96 overflow-y-auto">
+                <div className="max-h-96 overflow-y-auto">
                   <img
                     src="https://health.clevelandclinic.org/wp-content/uploads/sites/3/2022/04/exerciseHowOften-944015592-770x533-1-745x490.jpg"
                     alt="Exercise"
                     className="max-w-full h-auto md:max-w-md md:h-auto mx-auto"
                   />
-                  <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices molestie nulla ac tempus. Vivamus vestibulum metus diam, vel malesuada tellus mattis pharetra. Donec lacinia accumsan molestie. In commodo lobortis libero, a volutpat augue rutrum eu. Morbi posuere diam a nunc interdum pellentesque. Ut at erat at ipsum semper faucibus. Pellentesque scelerisque iaculis malesuada. Etiam scelerisque, quam sit amet varius semper, dolor dolor aliquam tellus, in rutrum libero lacus vitae turpis. Pellentesque auctor dolor vel mi lacinia, in aliquet nisi bibendum. Mauris eget molestie augue. Sed dignissim, elit ut vehicula condimentum, tellus dui aliquet tortor, ac iaculis ligula lectus non ante. Donec condimentum egestas risus, non rhoncus metus blandit non. Aliquam erat volutpat. In in dictum sem, in vulputate est.
-                  </p>
+                  <h3
+                    className="text-base font-semibold leading-6 text-black"
+                    id="modal-title"
+                  >
+                    Description
+                  </h3>
+                  <div className="text-sm text-gray-500">
+                    {exercise.description}
+                  </div>
+                  <div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices molestie nulla ac tempus. Vivamus vestibulum metus diam, vel malesuada tellus mattis pharetra. Donec lacinia accumsan molestie. In commodo lobortis libero, a volutpat augue rutrum eu. Morbi posuere diam a nunc interdum pellentesque. Ut at erat at ipsum semper faucibus. Pellentesque scelerisque iaculis malesuada. Etiam scelerisque, quam sit amet varius semper, dolor dolor aliquam tellus, in rutrum libero lacus vitae turpis. Pellentesque auctor dolor vel mi lacinia, in aliquet nisi bibendum. Mauris eget molestie augue. Sed dignissim, elit ut vehicula condimentum, tellus dui aliquet tortor, ac iaculis ligula lectus non ante. Donec condimentum egestas risus, non rhoncus metus blandit non. Aliquam erat volutpat. In in dictum sem, in vulputate est.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -53,14 +53,3 @@ function ExerciseModal({ isOpen, closeModal, exercise }: { isOpen: boolean, exer
 }
 
 export default ExerciseModal;
-
-
-
-
-
-
-
-
-
-
-
