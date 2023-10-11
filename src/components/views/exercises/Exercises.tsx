c6af843a51f561b5d36283c09d12028ebbffd18c
ErlingMMM <emagnusmoland@gmail.com>
2023-10-11 17:27:58 +0200

    css for search icon

--- src/components/views/exercises/Exercises.tsx
+++ src/components/views/exercises/Exercises.tsx
@@ -6,6 +6,9 @@ import { AnyAction } from '@reduxjs/toolkit';
 import { RootState } from '../../../Redux/Store';
 import { getExcersiceInfo } from '../../../Redux/GenericSlice';
 import loadingGif from '../../../assets/loading.gif';
+import { SearchIcon } from '@heroicons/react/outline';
+
+
 
 function Exercise() {
   const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
@@ -39,25 +42,25 @@ function Exercise() {
 
   const filteredExercises = Array.isArray(exercises)
     ? exercises.filter((exercise: any) => {
-        const query = searchQuery.toLowerCase();
-        const exerciseName = exercise.name.toLowerCase();
-        return exerciseName.includes(query);
-      })
+      const query = searchQuery.toLowerCase();
+      const exerciseName = exercise.name.toLowerCase();
+      return exerciseName.includes(query);
+    })
     : [];
 
 
   // Define dummy exercise images URLs
   const dummyImageUrls = [
-    'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',    
+    'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
     'https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
     'https://images.unsplash.com/photo-1574680178050-55c6a6a96e0a?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3269&q=80',
   ];
 
-    // Helper function to get a random dummy image URL
-    const getRandomDummyImageUrl = () => {
-      const randomIndex = Math.floor(Math.random() * dummyImageUrls.length);
-      return dummyImageUrls[randomIndex];
-    };
+  // Helper function to get a random dummy image URL
+  const getRandomDummyImageUrl = () => {
+    const randomIndex = Math.floor(Math.random() * dummyImageUrls.length);
+    return dummyImageUrls[randomIndex];
+  };
 
 
   return (
@@ -65,17 +68,20 @@ function Exercise() {
       <div>
         <br />
         <div className="text-center mt-4">
-  <input
-    className="border-2 border-gray-300 bg-white h-12 w-10/12 px-5 pr-16 rounded-3xl text-sm focus:outline-none inline-block"
-    type="text"
-    placeholder="search exercises"
-    value={searchQuery}
-    onChange={(e) => setSearchQuery(e.target.value)}
-  />
-</div>
-
-
-<br />
+          <div className="relative">
+            <input
+              className="border-2 border-gray-300 bg-white h-12 w-10/12 px-5 pr-16 rounded-3xl text-sm focus:outline-none inline-block"
+              type="text"
+              placeholder="Search exercises"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="absolute left-3 top-3 text-gray-400">
+              <SearchIcon className="w-5 h-5" />
+            </div>
+          </div>
+        </div>
+        <br />
         {isLoading ? (
           <div>
             <img src={loadingGif} alt="Loading..." />
@@ -84,24 +90,24 @@ function Exercise() {
         ) : (
           <ul>
             {Array.isArray(filteredExercises) && filteredExercises.length > 0 ? (
-filteredExercises.map((exercise: any) => (
-  <li key={exercise.id} className="mb-6">
- <button onClick={() => openModal(exercise)} className="flex items-start">
-<img
-src={getRandomDummyImageUrl()}
-alt={exercise.name}
-style={{ width: '100px', height: '100px', marginLeft: '30px', borderRadius: 18 }} // Adjust the width and height as needed
-/>
-<div>
-  <h3 className='text-lg font-bold' style={{ marginLeft: '-20px' }}>{exercise.name}</h3>
-    <p style={{ marginLeft: '-45px' }}>Level: {exercise.difficulty} </p>
-    <br />
-    <p style={{ marginLeft: '10px' }}>{exercise.muscleGroup}</p>
-  </div>
-</button>
+              filteredExercises.map((exercise: any) => (
+                <li key={exercise.id} className="mb-6">
+                  <button onClick={() => openModal(exercise)} className="flex items-start">
+                    <img
+                      src={getRandomDummyImageUrl()}
+                      alt={exercise.name}
+                      style={{ width: '100px', height: '100px', marginLeft: '30px', borderRadius: 18 }} // Adjust the width and height as needed
+                    />
+                    <div>
+                      <h3 className='text-lg font-bold' style={{ marginLeft: '-20px' }}>{exercise.name}</h3>
+                      <p style={{ marginLeft: '-45px' }}>Level: {exercise.difficulty} </p>
+                      <br />
+                      <p style={{ marginLeft: '10px' }}>{exercise.muscleGroup}</p>
+                    </div>
+                  </button>
 
-  </li>
-))
+                </li>
+              ))
 
             ) : (
               <div>
@@ -111,7 +117,6 @@ style={{ width: '100px', height: '100px', marginLeft: '30px', borderRadius: 18 }
           </ul>
         )}
       </div>
-
       <ExerciseModal
         isOpen={isModalOpen}
         closeModal={() => setIsModalOpen(false)}
