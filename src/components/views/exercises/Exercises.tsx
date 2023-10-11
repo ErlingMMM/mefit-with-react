9301ca4db54e34e68fccfa05681ad4547c2bac37
ErlingMMM <emagnusmoland@gmail.com>
2023-10-11 14:46:44 +0200

    adjusted the text margin

--- src/components/views/exercises/Exercises.tsx
+++ src/components/views/exercises/Exercises.tsx
@@ -48,9 +48,9 @@ function Exercise() {
 
   // Define dummy exercise images URLs
   const dummyImageUrls = [
-    'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2022/04/exerciseHowOften-944015592-770x533-1-745x490.jpg',
-    'https://info.totalwellnesshealth.com/hs-fs/hubfs/HealthBenefitsFitness.png?width=675&name=HealthBenefitsFitness.png',
-    'https://i0.wp.com/beautyleebar.com/wp-content/uploads/2023/05/istockphoto-1344876535-612x612-1.jpg?fit=437%2C612&ssl=1',
+    'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',    
+    'https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
+    'https://images.unsplash.com/photo-1574680178050-55c6a6a96e0a?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3269&q=80',
   ];
 
     // Helper function to get a random dummy image URL
@@ -80,17 +80,18 @@ function Exercise() {
           <ul>
             {Array.isArray(filteredExercises) && filteredExercises.length > 0 ? (
 filteredExercises.map((exercise: any) => (
-  <li key={exercise.id}>
+  <li key={exercise.id} className="mb-6">
  <button onClick={() => openModal(exercise)} className="flex items-start">
 <img
 src={getRandomDummyImageUrl()}
 alt={exercise.name}
-style={{ width: '100px', height: '100px' }} // Adjust the width and height as needed
+style={{ width: '100px', height: '100px', marginLeft: '30px', borderRadius: 18 }} // Adjust the width and height as needed
 />
-  <div style={{ marginLeft: '10px' }}>
-    <p>Level: {exercise.difficulty}</p>
-    <p>{exercise.name}</p>
-    <p>{exercise.muscleGroup}</p>
+<div>
+  <h3 className='text-lg font-bold' style={{ marginLeft: '-20px' }}>{exercise.name}</h3>
+    <p style={{ marginLeft: '-45px' }}>Level: {exercise.difficulty} </p>
+    <br />
+    <p style={{ marginLeft: '10px' }}>{exercise.muscleGroup}</p>
   </div>
 </button>
 
