e04dd77283a9a03d840995733869c736789ae1be
ErlingMMM <emagnusmoland@gmail.com>
2023-10-13 00:50:01 +0200

    removed code not in use

--- src/components/views/explorer/Explorer.tsx
+++ src/components/views/explorer/Explorer.tsx
@@ -1,22 +1,20 @@
 import { useState, useEffect } from 'react';
-import ExerciseModal from '../../modals/ExerciseModal';
 import DisplayManager from './DisplayManager';
 import SearchBar from './SearchBar';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from '@reduxjs/toolkit';
 import { RootState } from '../../../Redux/Store';
 import { getExcersiceInfo, getWorkoutInfo } from '../../../Redux/GenericSlice';
-import loadingGif from '../../../assets/loading.gif'; // Import the loading GIF
+import loadingGif from '../../../assets/loading.gif'; 
 
 function Explorer() {
   const [activeComponent, setActiveComponent] = useState('exercises');
   const [searchQuery, setSearchQuery] = useState('');
-  const [isLoading, setIsLoading] = useState(true); // Manage isLoading in Explorer
+  const [isLoading, setIsLoading] = useState(true); 
 
   const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
-  const exercises = useSelector((state: any) => state.data.exerciseData);
-  const workouts = useSelector((state: any) => state.data.workoutData);
+ 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,7 +43,7 @@ function Explorer() {
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} placeholder={placeholder} />
       <button onClick={() => switchToComponent('exercises')}>exercises</button>
       <button onClick={() => switchToComponent('workouts')}>workouts</button>
-      {isLoading ? ( // Conditionally render the loading GIF
+      {isLoading ? ( 
         <div>
           <img src={loadingGif} alt="Loading..." />
         </div>
