ca19cfed602c27894b8dd3e5815d50bb8e7bf084
ErlingMMM <emagnusmoland@gmail.com>
2023-10-12 13:02:56 +0200

    external input field onFocus styling

--- src/components/views/exercises/Exercises.tsx
+++ src/components/views/exercises/Exercises.tsx
@@ -70,20 +70,31 @@ function Exercise() {
         <br />
         <div className="text-center mt-4">
           <div className="relative">
-          <div className="custom-input border-2 border-gray-300 bg-white h-12 w-10/12 rounded-3xl text-sm focus:outline-none inline-block">
-          <input 
+            <div className="external-input-style border-2 border-gray-300 bg-white h-12 w-10/12 rounded-3xl text-sm focus:outline-none inline-block">
+              <input
                 type="text"
                 placeholder="Search exercises"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
-                onFocus={(e) => (e.target.placeholder = "")}
-                onBlur={(e) => (e.target.placeholder = "Search exercises")}
+                onFocus={(e) => {
+                  e.target.placeholder = "";
+                  if (e.target.parentElement) {
+                    e.target.parentElement.classList.add("focused");
+                  }
+                }}
+                onBlur={(e) => {
+                  e.target.placeholder = "Search exercises";
+                  if (e.target.parentElement) {
+                    e.target.parentElement.classList.remove("focused");
+                  }
+                }}
                 style={{ marginTop: '11px', marginRight: '60px' }}
               />
             </div>
             <div className="absolute left-4 top-4 text-gray-400">
               <SearchIcon className="w-4 h-4" />
             </div>
+
           </div>
         </div>
         <br />
@@ -101,8 +112,8 @@ function Exercise() {
                     <img
                       src={getRandomDummyImageUrl()}
                       alt={exercise.name}
-                      className="custom-image-style"                   
-                       />
+                      className="custom-image-style"
+                    />
                     <div>
                       <h3 className='text-lg font-bold' style={{ marginLeft: '-20px' }}>{exercise.name}</h3>
                       <p style={{ marginLeft: '-45px' }}>Level: {exercise.difficulty} </p>
