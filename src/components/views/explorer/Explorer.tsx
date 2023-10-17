95ccb033581a8eecdefb4a94d47bb0902fd9a37e
ErlingMMM <emagnusmoland@gmail.com>
2023-10-17 15:13:51 +0200

    buttons responsive

--- src/components/views/explorer/Explorer.tsx
+++ src/components/views/explorer/Explorer.tsx
@@ -7,6 +7,7 @@ import { AnyAction } from '@reduxjs/toolkit';
 import { RootState } from '../../../Redux/Store';
 import { getExcersiceInfo, getProgramInfo, getWorkoutInfo } from '../../../Redux/GenericSlice';
 import loadingGif from '../../../assets/loading.gif';
+import '../../../styles/Explorer.css';
 
 function Explorer() {
   const [activeComponent, setActiveComponent] = useState('programs');
@@ -14,7 +15,6 @@ function Explorer() {
   const [isLoading, setIsLoading] = useState(true);
 
   const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +28,7 @@ function Explorer() {
         }, 500);
       } catch (error) {
         console.error('Error fetching data:', error);
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     };
 
@@ -46,20 +46,38 @@ function Explorer() {
     : isExercisesView
     ? 'Search exercises'
     : 'Search workouts';
-  
 
   return (
     <>
       <br />
-        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} placeholder={placeholder} />
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} placeholder={placeholder} />
       <br />
-      <button onClick={() => switchToComponent('programs')}>programs</button>
-      <button onClick={() => switchToComponent('exercises')}>exercises</button>
-      <button onClick={() => switchToComponent('workouts')}>workouts</button>
+      <div className="flex justify-between w-full space-x-1">
+  <button
+    onClick={() => switchToComponent('programs')}
+    className="flex-1 pl-10 pr-4  py-2 text-lg  rounded-md"
+  >
+    Programs
+  </button>
+  <button
+    onClick={() => switchToComponent('exercises')}
+    className="flex-1 px-4 py-2 text-lg rounded-md"
+  >
+    Exercises
+  </button>
+  <button
+    onClick={() => switchToComponent('workouts')}
+    className="flex-1 pr-10 pl-4 py-2 text-lg rounded-md"
+  >
+    Workouts
+  </button>
+</div>
+
+
 
       {isLoading ? (
-        <div>
-          <img src={loadingGif} alt="Loading..." />
+        <div className="loading-container">
+          <img src={loadingGif} alt="Loading..." className="loading-image" />
         </div>
       ) : (
         <DisplayManager activeComponent={activeComponent} searchQuery={searchQuery} />
