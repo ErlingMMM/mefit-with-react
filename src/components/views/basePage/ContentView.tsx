c802d8b3c7a3183e9c7d160df6e7dfb8e57b9353
ErlingMMM <emagnusmoland@gmail.com>
2023-10-18 21:46:12 +0200

    rename folder

--- src/components/views/basePage/ContentView.tsx
+++ src/components/views/basePage/ContentView.tsx
@@ -1,8 +1,8 @@
 import Explorer from '../explorer/explore/Explorer';
 import Dashboard from '../dashboard/Dashboard';
 import Profile from '../profile/Profile';
-import WorkoutsInProgram from '../FilteredLists/WorkoutsInProgram';
-import ExercisesInWorkouts from '../FilteredLists/ExercisesInWorkouts';
+import WorkoutsInProgram from '../listsFiltered/WorkoutsInProgram';
+import ExercisesInWorkouts from '../listsFiltered/ExercisesInWorkouts';
 
 
 function ContentView({ activeComponent = 'dashboard' }: { activeComponent?: string }) {
