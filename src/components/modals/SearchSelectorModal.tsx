24a441c480936436d32dca82b93a3dcf00baebe7
ErlingMMM <emagnusmoland@gmail.com>
2023-10-19 03:28:06 +0200

    only not active options part of the buttons

--- src/components/modals/SearchSelectorModal.tsx
+++ src/components/modals/SearchSelectorModal.tsx
@@ -1,5 +1,5 @@
 import { XIcon } from '@heroicons/react/outline';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedSearchOption } from '../../Redux/GenericSlice';
 
 function toTitleCase(str: string) {
@@ -11,11 +11,14 @@ function toTitleCase(str: string) {
 
 function SearchSelectorModal({ isOpen, closeModal, availableSearchOptions }: { isOpen: boolean, closeModal: () => void, availableSearchOptions: string[] }) {
   const dispatch = useDispatch();
+  const selectedSearchOption = useSelector((state: any) => state.data.selectedSearchOption);
 
   const searchSelector = (option: string) => {
     dispatch(setSelectedSearchOption(option));
     closeModal();
   }
+
+  const filteredOptions = availableSearchOptions.filter(option => option !== selectedSearchOption);
 
   return (
     <div>
@@ -32,16 +35,16 @@ function SearchSelectorModal({ isOpen, closeModal, availableSearchOptions }: { i
           <div className="relative overflow-hidden rounded-lg bg-white text-center shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl max-h-screen">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-black">
               <XIcon
-                className="h-6 w-6 text-black hover:bg-gray-200 hover:bg-opacity-50 rounded-full cursor-pointer absolute left-3 top-3"
+                className="h-6 w-6 text-black hover:bg-gray-200 hover-bg-opacity-50 rounded-full cursor-pointer absolute left-3 top-3"
                 onClick={closeModal}
               />
             </div>
             <div className="p-4">
-              {availableSearchOptions.map((option) => (
+              {filteredOptions.map((option) => (
                 <button
                   key={option}
                   onClick={() => searchSelector(option)}
-                  className="block py-2 px-4 hover:bg-gray-400 rounded-md m-2 cursor-pointer"
+                  className="block py-2 px-4 rounded-md m-2 cursor-pointer"
                 >
                   {toTitleCase(option)}
                 </button>
