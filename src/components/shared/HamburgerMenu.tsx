eb6affa2a99f39d2ecff473ab9913723a547c6ec
ErlingMMM <emagnusmoland@gmail.com>
2023-10-19 21:51:45 +0200

    testing nav bar

--- src/components/shared/HamburgerMenu.tsx
+++ src/components/shared/HamburgerMenu.tsx
@@ -8,42 +8,61 @@ interface HamburgerMenuProps {
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ isOpen, toggleMenu }) => {
   return (
     <div className="relative mt-5">
+      {/* Hamburger button */}
       <button
-        className="text-black w-10 h-10 relative focus:outline-none bg-white"
+        className="text-black w-10 h-10 relative focus:outline-none bg-white z-50" // Set z-index to display above other content
         onClick={toggleMenu}
       >
         <div>
           <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <span
-  className={`block absolute h-0.5  bg-current transform transition duration-75 ease-in-out ${
-    isOpen ? 'rotate-45 w-5' : 'w-4'
-  }`}
-  style={{ left: '30px' }} 
-></span>
-<span
-  className={`block absolute h-0.5 bg-current transform transition duration-75 ease-in-out ${
-    isOpen ? '-rotate-45 w-5' : 'w-4'
-  }`}
-  style={{ left: '30px' }} 
-></span>
-
+            <span
+              className={`block absolute h-0.5 bg-current transform transition duration-75 ease-in-out ${
+                isOpen ? 'rotate-45 w-5' : 'w-4'
+              }`}
+              style={{ left: '30px' }}
+            ></span>
+            <span
+              className={`block absolute h-0.5 bg-current transform transition duration-75 ease-in-out ${
+                isOpen ? '-rotate-45 w-5' : 'w-4'
+              }`}
+              style={{ left: '30px' }}
+            ></span>
           </div>
           <div>
             <span
               className={`block absolute h-0.5 w-4 bg-current transform transition duration-75 ease-in-out ${
                 isOpen ? 'opacity-0' : ''
               }`}
-              style={{ top: '13px', left: '40px' }} 
+              style={{ top: '13px', left: '40px' }}
             ></span>
             <span
               className={`block absolute h-0.5 w-4 bg-current transform transition duration-75 ease-in-out ${
                 isOpen ? 'opacity-0' : ''
               }`}
-              style={{ top: '28px', left: '40px' }} 
+              style={{ top: '28px', left: '40px' }}
             ></span>
           </div>
         </div>
       </button>
+
+      {/* Side menu */}
+      <div
+        className={`fixed left-0 top-0 h-full w-1/2 bg-white transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 z-40`}// Set z-index to display above other content
+      >
+        <button
+          onClick={toggleMenu}
+          className="absolute top-5 right-5 text-black w-10 h-10 focus:outline-none"
+        >
+          X {/* Close button */}
+        </button>
+        <div className="mt-20 text-center">
+          {/* Place your navigation links here */}
+          <button >Dashboard</button>
+         
+        </div>
+      </div>
     </div>
   );
 };
