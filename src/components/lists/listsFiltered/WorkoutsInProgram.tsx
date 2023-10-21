92474219854aef6734460e6fbae856826673328a
ErlingMMM <emagnusmoland@gmail.com>
2023-10-22 01:46:30 +0200

    added left side margin for title and text

--- src/components/lists/listsFiltered/WorkoutsInProgram.tsx
+++ src/components/lists/listsFiltered/WorkoutsInProgram.tsx
@@ -20,76 +20,85 @@ function WorkoutsInProgram() {
 
 
   const products = useMemo(() => [
- 
     {
       id: 1,
       name: 'Product A',
       price: 19.99,
       image: 'https://images.unsplash.com/photo-1545575439-3261931f52f1?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'An affordable item priced at $19.99, ideal for everyday use.',
     },
     {
       id: 2,
       name: 'Product B',
       price: 29.99,
       image: 'https://images.unsplash.com/photo-1471506480208-91b3a4cc78be?auto=format&fit=crop&q=80&w=2074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'A quality product priced at $29.99, perfect for various occasions.',
     },
     {
       id: 3,
       name: 'Product C',
       price: 39.99,
       image: 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?auto=format&fit=crop&q=80&w=1925&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'A stylish item priced at $39.99, suitable for fashion enthusiasts.',
     },
     {
       id: 4,
       name: 'Product D',
       price: 49.99,
       image: 'https://images.unsplash.com/photo-1690230677207-126e472f37dc?auto=format&fit=crop&q=80&w=1975&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'A premium item priced at $49.99, great for luxury experiences.',
     },
     {
       id: 5,
       name: 'Product E',
       price: 59.99,
       image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'A versatile product priced at $59.99, adaptable for many uses.',
     },
     {
       id: 6,
       name: 'Product F',
       price: 69.99,
       image: 'https://images.unsplash.com/photo-1690373403498-a29da916ecf9?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'An elegant choice at $69.99, suitable for special events.',
     },
     {
       id: 7,
       name: 'Product G',
       price: 79.99,
       image: 'https://images.unsplash.com/photo-1591035897819-f4bdf739f446?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'A high-end item priced at $79.99, excellent for discerning tastes.',
     },
-
     {
       id: 8,
       name: 'Product H',
       price: 79.99,
-      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto.format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'Another $79.99 option, offering style and quality.',
     },
     {
       id: 9,
-      name: 'Product H',
+      name: 'Product I',
       price: 79.99,
-      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto.format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'Product H - An alternative choice at $79.99, balancing price and features.',
     },
     {
       id: 10,
-      name: 'Product H',
+      name: 'Product J',
       price: 79.99,
-      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto.format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'Product H - An additional variation at $79.99, providing more options.',
     },
     {
       id: 11,
-      name: 'Product H',
+      name: 'Product K',
       price: 79.99,
-      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'https://images.unsplash.com/photo-1693892014158-fdab425b0e1e?auto.format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      description: 'Product H - Yet another choice at $79.99, expanding your selection.',
     },
-
   ], []);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -140,7 +149,7 @@ function WorkoutsInProgram() {
     };
 
     fetchData();
-  }, [activeWorkout, products]); 
+  }, [activeWorkout, products]);
 
 
 
@@ -182,15 +191,23 @@ function WorkoutsInProgram() {
   return (
     <div>
       <br />
-         <img
+      <img
         className="h-44 w-screen"
         src={(workouts[activeWorkout] as { image: string } || {}).image || ''}
         alt="WorkoutImage"
       />
 
-<div>{workouts[activeWorkout].name}</div>
-<div>{workouts[activeWorkout].description}</div>
-     {/* <img
+      {/*
+      <div>{workouts[activeWorkout].name}</div>
+      <div>{workouts[activeWorkout].description}</div>
+*/}
+<div className='ml-11'>
+<div className='font-bold text-lg'>{products[activeWorkout].name}</div>
+      <div>{products[activeWorkout].description}</div>
+</div>
+     
+
+      {/* <img
         className="h-44 w-screen"
         src={(products[activeWorkout] as { image: string } || {}).image || ''}
         alt="WorkoutImage"
@@ -226,8 +243,8 @@ function WorkoutsInProgram() {
 
       <button onClick={handlePreviousNavigation}>Previous</button>
       <button onClick={() => handleNextNavigation()}>Next</button>
-<br />
-<br />
+      <br />
+      <br />
 
       <ExerciseList exercises={exercises} content={"explorer"} />
     </div>
