7490b8471c607b3403d7aa0e813c02e3381fce31
ErlingMMM <emagnusmoland@gmail.com>
2023-10-22 01:21:10 +0200

    moved the main title a bit down

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar: React.FC<NavbarProps> = () => {
       <div className="block sm:hidden relative">
         <HamburgerMenu isOpen={menuOpen} switchToComponent={switchToComponent} toggleMenu={() => setMenuOpen(!menuOpen)} page={"basePage"} />
         {menuOpen ? null : (
-          <h1 className="font-bold text-lg italic text-center absolute top-0 left-1/2 transform -translate-x-1/2">
+          <h1 className="font-bold text-lg italic text-center absolute top-2 left-1/2 transform -translate-x-1/2">
             MeFit
           </h1>
         )}
