3a351ff0cf480d0ec74a674f0f08aa23a964545d
ErlingMMM <emagnusmoland@gmail.com>
2023-10-23 00:25:27 +0200

    show more button complete

--- src/components/lists/ProgramsList.tsx
+++ src/components/lists/ProgramsList.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 
 interface Program {
@@ -16,6 +16,9 @@ interface ProgramsListProps {
 }
 
 function ProgramsList({ programs, onClick }: ProgramsListProps) {
+  const [showFullDescription, setShowFullDescription] = useState<boolean>(false);
+
+
   // Define dummy images URLs
   const dummyImageUrls = [
     'https://images.unsplash.com/photo-1574680096145-d05b474e2155?ixlib=rb-4.0.3&ixid=M3wxM[…]dlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3269&q=80',
@@ -29,9 +32,26 @@ function ProgramsList({ programs, onClick }: ProgramsListProps) {
     return dummyImageUrls[randomIndex];
   };
 
+  const truncateDescription = (description: string, maxLength: number) => {
+    if (description.length <= maxLength) {
+      return description;
+    }
+    const lastPunctuationIndex = Math.max(
+      description.lastIndexOf('.', maxLength),
+      description.lastIndexOf('?', maxLength),
+      description.lastIndexOf('!', maxLength)
+    );
+    if (lastPunctuationIndex >= 0) {
+      return description.slice(0, lastPunctuationIndex + 1);
+    }
+    // If no suitable punctuation is found, truncate at maxLength
+    return description.slice(0, maxLength);
+  };
+
+
   return (
     <div className="flex justify-center items-center">
-      <ul className="flex flex-wrap justify-center mx-9 md:grid  md:gap-4 md:grid-cols-2">
+      <ul className="flex flex-wrap justify-center mx-9 md:grid md:gap-4 md:grid-cols-2">
         {programs.map((program) => (
           <li key={program.id} className="mb-6">
             <button onClick={() => onClick(program.id)}>
@@ -47,9 +67,23 @@ function ProgramsList({ programs, onClick }: ProgramsListProps) {
                 <h3 className="text-lg font-bold">
                   {program.name}
                 </h3>
-                <p className="w-80">{program.description}</p> 
               </div>
             </button>
+            <div className="w-80 text-center"> {/* Center description and button */}
+              <p className={showFullDescription ? 'mt-4' : 'mt-2'}>
+                {showFullDescription
+                  ? program.description
+                  : truncateDescription(program.description, 200)}
+              </p>
+              {program.description.length > 200 && (
+                <button
+                  onClick={() => setShowFullDescription(!showFullDescription)}
+                  className="text-blue-500 cursor-pointer"
+                >
+                  {showFullDescription ? 'Show Less' : 'Show More'}
+                </button>
+              )}
+            </div>
           </li>
         ))}
       </ul>
