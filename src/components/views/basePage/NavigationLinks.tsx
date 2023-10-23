0fb4053176dbf35265effb43d2771e3a60729fa4
ErlingMMM <emagnusmoland@gmail.com>
2023-10-23 20:16:05 +0200

    top bar complete for base page

--- src/components/views/basePage/NavigationLinks.tsx
+++ src/components/views/basePage/NavigationLinks.tsx
@@ -15,22 +15,29 @@ const NavigationLinks: React.FC<NavigationLinksProps> = ({
   isContributor,
   onclickAdminContributor,
 }) => {
+  
   return (
-    <div className="sm:flex sm:space-x-4 nav-links hidden justify-between bg-black">
-      <div className="space-x-4 flex items-center ml-7">
-        <p className='text-custom-green italic text-lg'>Mefit</p>
-        <button className='text-white' onClick={() => switchToComponent('dashboard')}>Dashboard</button>
-        <button className='text-white' onClick={() => switchToComponent('profile')}>Profile</button>
-        <button className='text-white' onClick={() => switchToComponent('explorer')}>Explorer</button>
+    <div className="sm:flex sm:space-x-4 nav-links hidden justify-between bg-black py-2">
+      <div className="space-x-10 flex items-center ml-7 ">
+        <p className='text-custom-green italic text-lg font-sans font-bold'>Mefit</p>
+        <button className='text-white hover:text-custom-green' onClick={() => switchToComponent('dashboard')}>Dashboard</button>
+        <button className='text-white hover:text-custom-green' onClick={() => switchToComponent('explorer')}>Explorer</button>
         {(isAdmin || isContributor) && (
-          <button className='text-white' onClick={onclickAdminContributor}>Contributor</button>
+          <button className='text-white hover:text-custom-green' onClick={onclickAdminContributor}>Contributor</button>
         )}
       </div>
   
-      <div className="space-x-4 flex items-center ml-4 text-white">
-        <div>{keycloak.tokenParsed ? keycloak.tokenParsed.name : 'Unknown'}</div>
+      <div className="space-x-10 flex items-center ml-4 text-white ">
+        <div className='hover:text-custom-green text-xs'>{keycloak.tokenParsed ? keycloak.tokenParsed.name : 'Unknown'}</div>
+        <button className='text-custom-green text-center hover:opacity-70' onClick={() => switchToComponent('profile')} style={{ cursor: 'pointer', background: 'none', border: 'none', outline: 'none' }}>
+            <img
+              src="https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&q=80&w=1961&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="ProfilePage"
+              style={{ borderRadius: '50%', width: '50px', height: '50px', border: '3px solid' }}
+            />
+          </button>
         {keycloak.authenticated && (
-          <button className="pr-7" onClick={() => keycloak.logout()}>log out</button>
+          <button className="pr-7 hover:text-custom-green" onClick={() => keycloak.logout()}>log out</button>
         )}
       </div>
     </div>
