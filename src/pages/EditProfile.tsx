d5ab649b7b8c9c1c5de7c84c98cb716df205d2cf
SMpagi <mpagi99@gmail.com>
2023-10-23 19:31:54 +0200

    styling and  add mefit logo

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -75,9 +75,9 @@ function EditProfile() {
           height: UserHeight,
           weight: UserWeight,
           gender: UserGender,
-          //intensity:UserIntensityPreference,
-         // fitnessLvl: UserFitnessPreference,
-          //timeframe:UserDurationTimeFrame,
+          intensity:UserIntensityPreference,
+          fitnessLvl: UserFitnessPreference,
+         // timeframe:UserDurationTimeFrame,
         }))
         navigate('/');
         dispatch(setActiveComponent('profile'));
@@ -88,83 +88,88 @@ function EditProfile() {
   
 
   return (
-    <div className='bg-gray-500 h-screen flex flex-col justify-center items-center'>
-      <h1 className='text-3xl font-bold mb-4 text-white'>Profile Editing Page</h1>
+    <div className='bg-white h-screen flex flex-col justify-center items-center'>
+      <h1
+        className={`font-bold text-black text-2xl italic text-right absolute top-6 right-5`}
+      >
+        MeFit
+      </h1>
+     <h1 className='text-3xl sm:text-4xl font-bold mb-4 text-black justify-end'>edit info </h1>
+     <button onClick={onClickBack} className='bg-custom-green text-gray-800 font-bold py-2 px-4 rounded absolute top-4 left-4 sm:top-8 sm:left-8'>Back</button>
 
-      <button onClick={onClickBack} className='bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded absolute top-4 left-4'>Back</button>
-      <form className='bg-white p-8 rounded shadow-md'>
-        <label className='block mb-2 text-gray-800' htmlFor="bio">Bio:</label>
+      <form className="bg-white p-8 rounded">
+        <label className='block mb-2 text-black font-bold ' htmlFor="bio">Bio:</label>
         <input  onChange={handleBioChange} className='w-full p-2 mb-4 border rounded' type="text" id="bio" name="bio" />
 
-        <label className='block mb-2 text-gray-800' htmlFor="age">Age:</label>
+        <label className='block mb-2 text-gray-800 font-bold' htmlFor="age">Age:</label>
         <input  onChange={handleAgeChange} className='w-full p-2 mb-4 border rounded' type="text" id="age" name="age" />
 
-        <label className='block mb-2 text-gray-800' htmlFor="height">Height:</label>
+        <label className='block mb-2 text-gray-800 font-bold' htmlFor="height">Height:</label>
         <input onChange={handleHeightChange} className='w-full p-2 mb-4 border rounded' type="text" id="height" name="height" />
 
-        <label className='block mb-2 text-gray-800' htmlFor="weight">Weight:</label>
+        <label className='block mb-2 text-gray-800 font-bold' htmlFor="weight">Weight:</label>
         <input onChange={handleWeightChange} className='w-full p-2 mb-4 border rounded' type="text" id="weight" name="weight" />
 
-        <label className='block mb-2 text-gray-800'>Gender:</label>
+        <label className='block mb-2 text-gray-800 font-bold text-xl'>Gender:</label>
         <div className='flex items-center mb-4'>
-          <input  onChange={handleRadioChange} className='mr-2' type="radio" id="male" name="gender" value="male" />
-          <label className='text-gray-800' htmlFor="male">Male</label>
+          <input  onChange={handleRadioChange} className='mr-2 ' type="radio" id="male" name="gender" value="male" />
+          <label className='text-gray-800   text-xl' htmlFor="male">Male</label>
 
-          <input onChange={handleRadioChange} className='mx-2' type="radio" id="female" name="gender" value="female" />
-          <label className='text-gray-800' htmlFor="female">Female</label>
+          <input onChange={handleRadioChange} className='mx-2 font-bold' type="radio" id="female" name="gender" value="female" />
+          <label className='text-gray-800  text-xl' htmlFor="female">Female</label>
 
-          <input onChange={handleRadioChange} className='mx-2' type="radio" id="other" name="gender" value="other" />
-          <label className='text-gray-800' htmlFor="other" >Other</label>
+          <input onChange={handleRadioChange} className='mx-2 font-bold' type="radio" id="other" name="gender" value="other" />
+          <label className='text-gray-800   text-xl' htmlFor="other" >Other</label>
         </div>
 
-        <label className='block mb-2 text-gray-800'>Fitness preference:</label>
+        <label className='block mb-2 text-gray-800 font-bold text-xl'>Fitness preference:</label>
         <div className='flex items-center mb-4'>
-          <input  onChange={handleRadio2Change} className='mr-2' type="radio" id="male" name="gender" value="0" />
-          <label className='text-gray-800' htmlFor="male">Beginner</label>
+          <input  onChange={handleRadio2Change} className='mr-2' type="radio" id="male" name="gender" value="Beginner" />
+          <label className="text-gray-800 text-xl" htmlFor="male">Beginner</label>
 
-          <input onChange={handleRadio2Change} className='mx-2' type="radio" id="female" name="gender" value="1" />
-          <label className='text-gray-800' htmlFor="female">Intermediate</label>
+          <input onChange={handleRadio2Change} className='mx-2' type="radio" id="female" name="gender" value="Intermediate" />
+          <label className="text-gray-800  text-xl" htmlFor="female">Intermediate</label>
 
-          <input onChange={handleRadio2Change} className='mx-2' type="radio" id="other" name="gender" value="2" />
-          <label className='text-gray-800' htmlFor="other" >Advanced</label>
+          <input onChange={handleRadio2Change} className='mx-2' type="radio" id="other" name="gender" value="Expert" />
+          <label className="text-gray-800 text-xl"  htmlFor="other" >Advanced</label>
         </div>
 
-        <label className='block mb-2 text-gray-800'>Timeframe:</label>
+        <label className='block mb-2 text-gray-800 font-bold text-xl'>Timeframe:</label>
         <div className='flex items-center mb-4'>
           <input  onChange={handleRadio3Change} className='mr-2' type="radio" id="male" name="gender" value="15" />
-          <label className='text-gray-800' htmlFor="male">15 days</label>
+          <label className='text-gray-800  text-xl' htmlFor="male">15 days</label>
 
           <input onChange={handleRadio3Change} className='mx-2' type="radio" id="female" name="gender" value="30" />
-          <label className='text-gray-800' htmlFor="female">30 days</label>
+          <label className='text-gray-800  text-xl' htmlFor="female">30 days</label>
 
           <input onChange={handleRadio3Change} className='mx-2' type="radio" id="other" name="gender" value="45" />
-          <label className='text-gray-800' htmlFor="other" >45 days</label>
+          <label className='text-gray-800  text-xl' htmlFor="other" >45 days</label>
         </div>
 
-        <label className='block mb-2 text-gray-800'>intensity (workouts per week - wpw)</label>
+        <label className='block mb-2 text-gray-800 font-bold text-xl'>intensity (workouts per week - wpw)</label>
         <div className='flex items-center mb-4'>
           
           <input  onChange={handleRadio4Change} className='mr-2' type="radio" id="male" name="gender" value="1" />
-          <label className='text-gray-800' htmlFor="male">1 wpw</label>
+          <label className='text-gray-800 text-xl' htmlFor="male">1 a/w </label>
 
           <input onChange={handleRadio4Change} className='mx-2' type="radio" id="female" name="gender" value="2" />
-          <label className='text-gray-800' htmlFor="female">2 wpw</label>
+          <label className='text-gray-800  text-xl' htmlFor="female">2 a/w  </label>
 
           <input onChange={handleRadio4Change} className='mx-2' type="radio" id="other" name="gender" value="3" />
-          <label className='text-gray-800' htmlFor="other" >3 wpw</label>
+          <label className='text-gray-800  text-xl' htmlFor="other" >3 a/w </label>
           <br />
           <input onChange={handleRadio4Change} className='mx-2' type="radio" id="other" name="gender" value="4" />
-          <label className='text-gray-800' htmlFor="other" >4 wpw</label>
+          <label className='text-gray-800 text-xl' htmlFor="other" >4  a/w</label>
 
           <input onChange={handleRadio4Change} className='mx-2' type="radio" id="other" name="gender" value="5" />
-          <label className='text-gray-800' htmlFor="other" >5 wpw</label>
+          <label className='text-gray-800  text-xl' htmlFor="other" >5  a/w</label>
         </div>
 
-        <button onClick={handleSave} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type="submit">Save</button>
+        <button onClick={handleSave} className="w-full bg-custom-green text-white font-bold py-2 px-4 rounded-lg focus:ring focus:ring-green-200" type="submit">Save</button>
 
       </form>
     </div>
   );
-}
+} 
 
 export default EditProfile;
