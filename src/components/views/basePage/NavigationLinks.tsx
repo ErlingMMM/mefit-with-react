f9740b29e17a9c611b165bca774a3f809f35be60
ErlingMMM <emagnusmoland@gmail.com>
2023-10-26 03:14:23 +0200

    diable hover when active component

--- src/components/views/basePage/NavigationLinks.tsx
+++ src/components/views/basePage/NavigationLinks.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import { useState } from 'react';
 import keycloak from "../../../Keycloak";
 import { useSelector } from "react-redux";
+
 
 
 interface NavigationLinksProps {
@@ -17,21 +18,52 @@ const NavigationLinks: React.FC<NavigationLinksProps> = ({
   onclickAdminContributor,
 }) => {
   const user = useSelector((state: any) => state.data.userData);
+  const [currentComponent, setCurrentComponent] = useState('dashboard');
+
+  const handleClick = (component: string) => {
+    setCurrentComponent(component);
+    switchToComponent(component);
+  }
 
   return (
 <div className="sm:flex sm:space-x-4 nav-links hidden justify-between bg-black py-2 ring-2 dark:ring-[#A8E52D] dark:ring-2">
       <div className="space-x-10 flex items-center ml-7 ">
         <p className='text-custom-green italic text-lg font-sans font-bold hover:text-white cursor-pointer' onClick={() => switchToComponent('dashboard')}>Mefit</p>
-        <button className='text-white hover:text-custom-green' onClick={() => switchToComponent('dashboard')}>Dashboard</button>
-        <button className='text-white hover:text-custom-green' onClick={() => switchToComponent('explorer')}>Explorer</button>
-        {(isAdmin || isContributor) && (
-          <button className='text-white hover:text-custom-green' onClick={onclickAdminContributor}>Contributor</button>
-        )}
+        <button
+  className={`text-white hover:text-custom-green ${
+    currentComponent === 'dashboard' ? 'disable-hover' : ''
+  }`}
+  onClick={() => handleClick('dashboard')}
+  disabled={currentComponent === 'dashboard'}
+>
+  Dashboard
+</button>
+<button
+  className={`text-white hover:text-custom-green ${
+    currentComponent === 'explorer' ? 'disable-hover' : ''
+  }`}
+  onClick={() => handleClick('explorer')}
+  disabled={currentComponent === 'explorer'}
+>
+  Explorer
+</button>
+{(isAdmin || isContributor) && (
+  <button
+    className={`text-white hover:text-custom-green ${
+      currentComponent === 'contributor' ? 'disable-hover' : ''
+    }`}
+    onClick={onclickAdminContributor}
+    disabled={currentComponent === 'contributor'}
+  >
+    Contributor
+  </button>
+)}
+
       </div>
   
       <div className="space-x-10 flex items-center ml-4 text-white ">
         <div className='text-xs'>{keycloak.tokenParsed ? keycloak.tokenParsed.name : 'Unknown'}</div>
-        <button className='text-custom-green text-center' onClick={() => switchToComponent('profile')} style={{ cursor: 'pointer', background: 'none', border: 'none', outline: 'none' }}>
+        <button className='text-custom-green text-center' onClick={() => handleClick('profile')} style={{ cursor: 'pointer', background: 'none', border: 'none', outline: 'none' }}>
             <img src={user.picture ? user.picture : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"}
                         alt="ProfilePage"
               style={{ borderRadius: '50%', width: '50px', height: '50px', border: '2px solid' }}
