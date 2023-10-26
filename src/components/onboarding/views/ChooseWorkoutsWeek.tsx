535eecf64ffe4a9dfc94aaca21803f885f9dd5dd
ErlingMMM <emagnusmoland@gmail.com>
2023-10-26 18:44:58 +0200

    styling update

--- src/components/onboarding/views/ChooseWorkoutsWeek.tsx
+++ src/components/onboarding/views/ChooseWorkoutsWeek.tsx
@@ -31,36 +31,37 @@ function ChooseWorkoutsWeek() {
           <h1 className="text-xl font-bold mb-6 sm:text-2xl md:text-3xl w-64 sm:w-80 md:w-96">
             How many workouts do you want to do per week?
           </h1>
+          <br />
         </div>
       </div>
 
       <div>
         <form className="">
-          <div className="custom-radio mb-2 p-4 rounded flex items-start">
+          <div className="custom-radio  p-4 rounded flex items-start">
             <input type="radio" id="1workout" name="workoutsWeek" value="1 workout per week" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
             <label htmlFor="1workout" className="text-lg font-bold">1 workout per week</label>
             <p>For a light exercise routine.</p>
           </div>
 
-          <div className="custom-radio mb-2 p-4 rounded flex items-start">
+          <div className="custom-radio p-4 rounded flex items-start">
             <input type="radio" id="2workouts" name="workoutsWeek" value="2 workouts per week" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
             <label htmlFor="2workouts" className="text-lg font-bold">2 workouts per week</label>
             <p>For a moderate workout commitment.</p>
           </div>
 
-          <div className="custom-radio mb-2 p-4 rounded flex items-start">
+          <div className="custom-radio p-4 rounded flex items-start">
             <input type="radio" id="3workouts" name="workoutsWeek" value="3 workouts per week" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
             <label htmlFor="3workouts" className="text-lg font-bold">3 workouts per week</label>
             <p>For a dedicated fitness schedule.</p>
           </div>
 
-          <div className="custom-radio mb-2 p-4 rounded flex items-start">
+          <div className="custom-radio p-4 rounded flex items-start">
             <input type="radio" id="4workouts" name="workoutsWeek" value="4 workouts per week" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
             <label htmlFor="4workouts" className="text-lg font-bold">4 workouts per week</label>
             <p>For a dedicated fitness schedule.</p>
           </div>
 
-          <div className="custom-radio mb-2 p-4 rounded flex items-start">
+          <div className="custom-radio p-4 rounded flex items-start">
             <input type="radio" id="5workouts" name="workoutsWeek" value="5 workouts per week" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
             <label htmlFor="5workouts" className="text-lg font-bold">5 workouts per week</label>
             <p>For an intense and rigorous fitness routine.</p>
