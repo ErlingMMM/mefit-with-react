f92d978b81abb66fb93e3029a61613e142869cf2
ErlingMMM <emagnusmoland@gmail.com>
2023-10-26 14:00:52 +0200

    green onclick buttons

--- src/components/onboarding/views/ChooseDifficulty.tsx
+++ src/components/onboarding/views/ChooseDifficulty.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 function ChooseDifficulty() {
   const dispatch: ThunkDispatch<RootState, any, AnyAction> = useDispatch();
-  const [selectedOption, setSelectedOption] = useState('option1');
+  const [selectedOption, setSelectedOption] = useState('Beginner');
   const navigate = useNavigate();
   const intensityLevelUser = useSelector((state: any) => state.data.userData.timesAWeek);
   const fintessLevelBruker = useSelector((state: any) => state.data.userData.fitnessPreference);
@@ -34,63 +34,22 @@ function ChooseDifficulty() {
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-4 overflow-hidden">
-    <div className="mt-10">
-  <div>
-    <h1 className={`font-bold text-black text-2xl italic text-center`}>
-      MeFit
-    </h1>
-    <br />
-    <h1 className="text-lg font-bold mb-6 text-right ">What is your<br />current fitness<br />level?</h1>
-  </div>
-</div>
-     
+      {/* ... */}
+      <form className="">
+        <div className="custom-radio">
+          <input type="radio" id="Beginner" name="fitnessLevel" value="Beginner" onChange={handleRadioChange} checked={selectedOption === 'Beginner'} />
+          <label htmlFor="Beginner" className="text-lg font-bold">Beginner</label>
 
-     <div>
-      
-     </div>
+          <input type="radio" id="Intermediate" name="fitnessLevel" value="Intermediate" onChange={handleRadioChange} checked={selectedOption === 'Intermediate'} />
+          <label htmlFor="Intermediate" className="text-lg font-bold">Intermediate</label>
 
-    <form className="">
-        <div className="mb-2 p-4 rounded flex items-start">
-            <input type="radio" id="Beginner" name="fitnessLevel" value="Beginner" onChange={handleRadioChange} className="mr-2 h-8 w-8"  />
-            <div>
-            <label htmlFor="Beginner" className="text-lg font-bold">Beginner</label>
-            <p>Perfect for fitness newcomers.<br />
-            Covers fundamental exercises <br />
-            and techniques.</p>
-            </div> 
+          <input type="radio" id="Expert" name="fitnessLevel" value="Expert" onChange={handleRadioChange} checked={selectedOption === 'Expert'} />
+          <label htmlFor="Expert" className="text-lg font-bold">Expert</label>
         </div>
-
-        <div className="mb-2 p-4 rounded flex items-start">
-    <input type="radio" id="Intermediate" name="fitnessLevel" value="Intermediate" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
-    <div>
-        <label htmlFor="Intermediate" className="text-lg font-bold inline-block mb-1">Intermediate</label>
-        <p className="mb-0">For users with some prior fitness experience.<br />
-        Introduces more challenging workouts <br /> and exercises and techniques.</p>
+      </form>
     </div>
-
-    </div>
-
-    <div className="mb-2 p-4 rounded flex items-start">
-            <input type="radio" id="Expert" name="fitnessLevel" value="Expert" onChange={handleRadioChange} className="mr-2 h-8 w-8" />
-            <div>
-            <label htmlFor="Expert" className="text-lg font-bold">Expert</label>
-            <p>For seasoned fitness enthusiasts,<br />
-            the expert level offers advanced workouts <br />
-            and intense routines.</p>
-            </div>
-        </div>
-
-    </form>
-    <button className="underline" type="submit" onClick={handleRegistrerClick}>skip</button>
-
-
-</div>
-
-
-  
-
   );
-};
+}
 
 export default ChooseDifficulty;
 
