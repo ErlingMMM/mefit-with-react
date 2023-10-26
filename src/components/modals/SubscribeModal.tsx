48413c9f3fe08e78ac6d4d5b299ad7da5533a72d
ErlingMMM <emagnusmoland@gmail.com>
2023-10-26 02:23:12 +0200

    subscribe button animation

--- src/components/modals/SubscribeModal.tsx
+++ src/components/modals/SubscribeModal.tsx
@@ -104,9 +104,15 @@ function SubscribeModal({ isOpen, closeModal, id }: { isOpen: boolean, id: numbe
                 {isSubscribed ? null : (
                   <button onClick={() => subscribe(id)}
 
-                    className="block pb-2 pt-3 sm:mb-5 px-4 rounded-md text-custom-black font-bold sm:m-2 cursor-pointer hover:bg-gray-50 border-2 border-[#A8E52E] hover:border-[#D0FF70] items-center"
+                  className="w-full font-bold sm:py-2 py-6 rounded-lg relative block leading-tight ease-in overflow-hidden"
                   >
-                    Subscribe
+                    <div className="relative text-lg hover:text-white  group transition-color duration-700 italic text-black">
+                      <span className="absolute -inset-6 rounded-lg bg-custom-green"></span>
+    
+                      <span className="absolute left-0 w-0 sm:w-[47rem] h-36  bg-black transition-all duration-300 origin-left transform -translate-x-full -translate-y-2 ease-in group-hover:translate-x-0 -translate-y-4">
+                      </span> 
+                      <span className="relative">Subscribe</span>
+                    </div>
                   </button>
                 )}
               </div>
